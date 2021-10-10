c7d19c0e35afac496c662cbd9d1236b7a0570cd3
Dzhavid Eldarov <Dzhavid.Eldarov@rosbank.ru>
2021-10-10 20:59:12 +0300

    Реализована фунция для задачи 2SUM(https://contest.yandex.ru/contest/26365/problems/D/?success=54317691#51450/2021_01_19/3I9jnwYAEg)

---
+++ src/com/company/recursion/TwoSum.java
@@ -0,0 +1,39 @@
+package com.company.recursion;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int lenghtOfArray = s.nextInt();
+        int[] mas = new int[lenghtOfArray];
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = s.nextInt();
+        }
+        int found = s.nextInt();
+        foundSum(found, mas);
+      //  System.out.println(-56 + (-32));
+    }
+
+    static void foundSum(int x, int[] mas) {
+        int[] indexes = new int[2];
+        boolean r = false;
+            for (int i = 0; i < mas.length; i++) {
+                for (int j = i+1; j < mas.length; j++) {
+                    if (mas[i] + mas[j] == x) {
+                        indexes[0] = mas[i];
+                        indexes[1] = mas[j];
+                        r = true;
+                    }
+                }
+            }
+
+
+        //String result = (r) ? indexes[1] +" "+ indexes[0]:"None";
+        System.out.println((r) ? indexes[1] + " " + indexes[0] : "None");
+      //  System.out.println(indexes[1] +" "+ indexes[0]);
+    }
+}
+
+
